45b52761444bcd58348b2d593bfb0a93e4ed28e2
Bladen Anderson <Anderson-B7@ulster.ac.uk>
2024-10-08 14:41:21 +0100

    Added If statement

--- CalculatorNew/Program.cs
+++ CalculatorNew/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CalculatorApp()
+{
+    // Declare the variables and initialise them to 0
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
+    // Ask the user to input first number
+    Console.WriteLine("Type in the first number followed by the enter key");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
+
+    // Ask the user to input second number
+    Console.WriteLine("Type in the second number followed by the enter key");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+
+    // Use a loop to make the relevant decision
+    // and perform the requested maths operation
+    Console.WriteLine("Choose an option from the following list:");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Divide");
+    Console.WriteLine("4 - Multiply");
+    // Convert string to integer
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    // Use an IF statement to perform selected maths operation
+    if (choice == 1)
+    {
+        result = firstNumber + secondNumber;
+        Console.WriteLine($"Adding {firstNumber} and {secondNumber} equals {result}");
+    }
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtracting {secondNumber} from {firstNumber} equals {result}");
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} by {secondNumber} equals {result}");
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine($"Multiplying {firstNumber} by {secondNumber} equals {result}");
+    }
+    else
+    {
+        Console.WriteLine("You did not select a valid number between 1-4");
+    }
+}
+
+CalculatorApp();
